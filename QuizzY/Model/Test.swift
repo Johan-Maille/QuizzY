e380db1d60282c51aac93ab7b3b0ec68e293652c
Toularastel Nils <b0769@hotmail.fr>
2018-12-03 11:11:18 +0100

    debut classe test

---
+++ QuizzY/Model/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  QuizzY
+//
+//  Created by Nils Toularastel on 03/12/2018.
+//  Copyright © 2018 Johan Maille--Guilloux. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    var imagePath = 
+}
